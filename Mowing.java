d86f92f2fd7918963327cf4c75946840f4432579
Jake Armendariz <jakearmendariz99@gmail.com>
2020-03-24 11:47:33 -0700

    Reorganizd repository for git

--- Mowing.java
+++ Mowing.java
@@ -1,5 +1,3 @@
-package sideProjects;
-
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -22,7 +20,7 @@ public class Mowing {
 	private ArrayList<Point> list;
 	private GridBagConstraints layout;
 	private JLabel[][] grid;
-	
+
 	public Mowing() throws FileNotFoundException {
 		list = new ArrayList<Point>();
 		FileReader fr = new FileReader("mowInput.txt");
@@ -30,21 +28,21 @@ public class Mowing {
 		l = a.nextInt();
 		w = a.nextInt();
 		lawn = new char[l][w];
-		System.out.println("Dimensions: " +  l  + ", " +w );
+		System.out.println("Dimensions: " + l + ", " + w);
 		x = a.nextInt();
 		y = a.nextInt();
 		a.nextLine();
-		System.out.println("Starting pos: (" +  x  + ", " +y + ")" );
-		for(int i = 0; i < l; i++) {
+		System.out.println("Starting pos: (" + x + ", " + y + ")");
+		for (int i = 0; i < l; i++) {
 			String s = a.nextLine().replace(" ", "");
-			for(int j = 0; j < w; j++) {
+			for (int j = 0; j < w; j++) {
 				lawn[i][j] = s.charAt(j);
 			}
 		}
-		
+
 		JFrame frame = new JFrame("lawn");
 		JPanel jp = new JPanel();
-		//frame.setSize(400, 400);
+		// frame.setSize(400, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setBackground(new Color(100, 100, 255));
@@ -52,79 +50,73 @@ public class Mowing {
 		layout = new GridBagConstraints();
 		layout.insets = new Insets(0, 0, l, w);
 		grid = new JLabel[l][w];
-		
-		for(int i = 0; i < l; i ++) {
-			for(int j = 0; j < w; j++) {
+
+		for (int i = 0; i < l; i++) {
+			for (int j = 0; j < w; j++) {
 				layout.gridy = i;
 				layout.gridx = j;
 				JLabel jl;
-				if(lawn[i][j] == '.') {
+				if (lawn[i][j] == '.') {
 					jl = new JLabel("   ");
-				}
-				else {
+				} else {
 					jl = new JLabel("   ");
 				}
 				jl.setOpaque(true);
-				if(lawn[i][j] == 'T') {
+				if (lawn[i][j] == 'T') {
 					jl.setBackground(Color.black);
-				}
-				else if(lawn[i][j] == 'C') {
+				} else if (lawn[i][j] == 'C') {
 					jl.setBackground(Color.green);
-				}
-				else
+				} else
 					jl.setBackground(Color.blue);
 				grid[i][j] = jl;
 				jp.add(jl, layout);
-				
+
 			}
 		}
-		
+
 		frame.add(jp);
 		frame.pack();
-		
-	
+
 		play();
-		
-		
+
 	}
-	
-	
+
 	public void play() {
-			list.add(new Point(x, y));
-			printLawn();
-			while(list.size() != 0) {
-				for(int please = 0; please < list.size(); please++) {
-					Point p = list.get(please);
-					if(isClear((int)p.getX(), (int)p.getY())) {
-						for(int row = -1; row <= 1; row++) {
-							for(int col = -1; col <= 1; col++) {
-								if(lawn[(int)p.getX() + row][ (int)p.getY() + col] != 'C') {
-									//printLawn();
-									list.add(new Point((int)p.getX() + row,  (int)p.getY() + col ));
-									lawn[(int)p.getX() + row][ (int)p.getY() + col] = 'C';
-									grid[(int)p.getX() + row][ (int)p.getY() + col].setBackground(Color.green);
-									TimeUnit name = TimeUnit.MILLISECONDS;
-									try {
-										name.sleep(20);
-									} catch (InterruptedException e) {
-										e.printStackTrace();
-									}
+		list.add(new Point(x, y));
+		printLawn();
+		while (list.size() != 0) {
+			for (int please = 0; please < list.size(); please++) {
+				Point p = list.get(please);
+				if (isClear((int) p.getX(), (int) p.getY())) {
+					for (int row = -1; row <= 1; row++) {
+						for (int col = -1; col <= 1; col++) {
+							if (lawn[(int) p.getX() + row][(int) p.getY() + col] != 'C') {
+								// printLawn();
+								list.add(new Point((int) p.getX() + row, (int) p.getY() + col));
+								lawn[(int) p.getX() + row][(int) p.getY() + col] = 'C';
+								grid[(int) p.getX() + row][(int) p.getY() + col].setBackground(Color.green);
+								TimeUnit name = TimeUnit.MILLISECONDS;
+								try {
+									name.sleep(20);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
 								}
 							}
 						}
 					}
-					list.remove(please);
 				}
-				//System.out.println("List size: " + list.size());
+				list.remove(please);
 			}
-			System.out.println("Counter: " + counter);
-			
+			// System.out.println("List size: " + list.size());
+		}
+		System.out.println("Counter: " + counter);
+
 	}
-	
+
 	public void printLawn() {
-		for(int i = 0; i < l; i++) {
-			for(int j = 0; j < w; j++) {
-				//System.out.println("x : " + i + " y: " +j);
+		for (int i = 0; i < l; i++) {
+			for (int j = 0; j < w; j++) {
+				// System.out.println("x : " + i + " y: " +j);
 				System.out.print(lawn[i][j] + " ");
 			}
 			System.out.println();
@@ -134,26 +126,25 @@ public class Mowing {
 		System.out.println();
 
 	}
-	
-	
+
 	public boolean isClear(int x1, int y1) {
 		counter++;
-		if( x1 < l-1 && y1 < w-1 && x1 > 0 && y1 > 0) {
-			if(lawn[x1][y1] != 'T' && lawn[x1-1][y1-1] != 'T' && lawn[x1-1][y1] != 'T' && lawn[x1+1][y1-1] != 'T'  && lawn[x1-1][y1+1] != 'T' && lawn[x1+1][y1+1] != 'T' &&
-					lawn[x1+1][y1] != 'T' && lawn[x1][y1+1] != 'T'  && lawn[x1-1][y1] != 'T'  && lawn[x1][y1-1] != 'T') {
+		if (x1 < l - 1 && y1 < w - 1 && x1 > 0 && y1 > 0) {
+			if (lawn[x1][y1] != 'T' && lawn[x1 - 1][y1 - 1] != 'T' && lawn[x1 - 1][y1] != 'T'
+					&& lawn[x1 + 1][y1 - 1] != 'T' && lawn[x1 - 1][y1 + 1] != 'T' && lawn[x1 + 1][y1 + 1] != 'T'
+					&& lawn[x1 + 1][y1] != 'T' && lawn[x1][y1 + 1] != 'T' && lawn[x1 - 1][y1] != 'T'
+					&& lawn[x1][y1 - 1] != 'T') {
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
-				
-		}
-		else
+
+		} else
 			return false;
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		Mowing m = new Mowing();
-		//System.out.println("Program over");
+		// System.out.println("Program over");
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# JakesProjects
-#I am trying to turn this into a place where I can store my programming projects
+# Mowing: A Recursive Pathfinder
+
+My first recursive project in the beginning of 2018. I added a GUI component later that year, the blocks don't fill the screen, but it reads an array of a text file and shows blue blocks spreading over green (grass) and avoiding the black blocks (trees).
